b51738772100bb4c98a659c462178e6d966555c5
German X <german.khokhlov@gmail.com>
2025-06-17 12:10:09 +0300

    Update astro.config.mjs
    gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()]
+
+  site: 'https://germanx.github.io',
+  base: '/astro-blog',
 });
